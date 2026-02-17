1f2dcefec6aa6d4fd12972332321980320c50c3c
Bezaress <carolinapebez@gmail.com>
2026-02-17 11:50:01 -0600

    mi first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// NAVBAR INTERACCIÓN
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
